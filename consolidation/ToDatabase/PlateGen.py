eef97528b2fce4215fd395039bc8ca55b8432f04
afarinaligand <afarina@ligand.com>
2024-02-06 11:43:00 -0800

    Updated initial graph schema

--- consolidation/ToDatabase/PlateGen.py
+++ consolidation/ToDatabase/PlateGen.py
@@ -12,24 +12,26 @@ class getPlateDB:
         self.plate_type = plate_type  # Use a private variable to store plate_type
 
 
-    def get_plates_property(self, node_id, plate_type, uri, username, password):
+    def get_plates_property(self, node_id, uri, username, password):
         query = (
-            f"MATCH (node:{node_id} {{Plates: '{plate_type}'}}) "
-            "RETURN node.Plates AS platesProperty"
+        f"MATCH (node:{node_id}) "
+        "RETURN node.Plates AS platesProperty"
         )
 
         
+        plates_property_list = []  # Initialize an empty list to store results
+
         with GraphDatabase.driver(uri, auth=(username, password)) as driver:
             with driver.session() as session:
                 result = session.run(query)
 
-                
-
-                # Extract the platesProperty from the result
+                # Extract the platesProperty from the result and add to the list
                 for record in result:
                     plates_property = record["platesProperty"]
-        
-        self.PlateCatalogue = plates_property
+                    plates_property_list.extend(plates_property)  # Extend the list
+
+
+        self.PlateCatalogue = plates_property_list
 
 
 #--this will generate a random set of numbers and extensions to generate barcodes
@@ -44,20 +46,23 @@ class generate_random_numbers_extensions:
         extension = self.extension
         plates_to_generate = self.PlateNumber  # Number of plates to generate
         plates_generated = 0  # Counter for plates generated
-
+        new_plates = []
         while plates_generated < plates_to_generate:
             # Generate a random number
             new_number = random.randint(1, 999999)
+            
 
             # Format the number as 'R' followed by a zero-padded 6-digit numeric value
             formatted_number = f'{extension}{new_number:06d}'
+            
 
             # Check if the number has already been used
             if formatted_number not in used_numbers:
                 used_numbers.append(formatted_number)
+                new_plates.append(formatted_number)
                 plates_generated += 1  # Increment the counter
 
-        return used_numbers
+        return used_numbers, new_plates
     
 #--add to nodes in the database
 class addPlatesToDB:
@@ -65,6 +70,7 @@ class addPlatesToDB:
         self.node_id = node_id  # Use a private variable to store node_id
         self.plate_type = plate_type  # Use a private variable to store plate_type
         self.PlateCatalogue = PlateCatalogue  # current list of plates
+        
 
     def replace_list_property(self, uri, username, password):
         query = (
